4cad0337c82b67153935d2c66f44a0e466d574dc
SpikeMoon <spikemoon2491@gmail.com>
2021-12-10 14:07:14 +0100

    Create ShipOverlapingValidator.cs
    validator

---
+++ Battleship.Ascii/ShipOverlapingValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Battleship.GameController;
+using Battleship.GameController.Contracts;
+
+namespace Battleship.Ascii
+{
+    public static class ShipOverlapingValidator
+    {
+        public static bool ValidateShippingOverlap(IEnumerable<Position> positions, IEnumerable<Ship> myFleet)
+        {
+            foreach (var myShip in myFleet)
+            {
+                foreach (var position in positions)
+                {
+                    foreach (var check_ship_position in myShip.Positions) {
+                        if (position.Equals(check_ship_position)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
